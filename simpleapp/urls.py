f05c29a61fd7cd623184ca6d70c502c5c53f6737
Alex <4422504a@gmail.com>
2023-08-27 21:33:06 +0300

    '2023.08.27'

---
+++ simpleapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from .views import *
+
+
+urlpatterns = [
+
+   path('', ProductsList.as_view(), name='products'),
+   path('<int:pk>', ProductDetail.as_view(), name='product'),
+   path('create/', ProductCreate.as_view(), name='product_create'),
+   path('<int:pk>/update/', ProductUpdate.as_view(), name='product_update'),
+   path('<int:pk>/delete/', ProductDelete.as_view(), name='product_delete'),
+
+]
